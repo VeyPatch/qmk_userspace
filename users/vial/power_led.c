05601c80301e52a8ee209328bb0e109256bbd778
paatchii <50637577+paatchii@users.noreply.github.com>
2024-08-21 19:39:40 +0200

    add helix

---
+++ users/vial/power_led.c
@@ -0,0 +1,7 @@
+void keyboard_pre_init_user(void) {
+  // Set our LED pin as output
+  gpio_set_pin_output(24);
+  // Turn the LED off
+  // (Due to technical reasons, high is off and low is on)
+  gpio_write_pin_high(24);
+}
