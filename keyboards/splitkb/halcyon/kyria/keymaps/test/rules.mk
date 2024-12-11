f9a7474744029477d54434ee03f9cf70c43c1652
VeyPatch <126267034+VeyPatch@users.noreply.github.com>
2024-12-11 16:44:16 +0100

    add test

---
+++ keyboards/splitkb/halcyon/kyria/keymaps/test/rules.mk
@@ -0,0 +1,8 @@
+# This adds module functionality to your keyboard (files found in users/halcyon_modules)
+USER_NAME := halcyon_modules
+
+# Include the display.c file during compilation
+SRC += display.c
+
+# Include the font file
+SRC += graphics/thintel15.qff.c
